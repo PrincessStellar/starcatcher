2dc41260569d9b368a4e92049781a71b1017e1d1
Nikdo53 <usivatel@gmail.com>
2026-01-22 22:20:31 +0100

    Fix Concurrent modification crash

--- src/main/java/com/wdiscute/starcatcher/minigame/FishingMinigameScreen.java
+++ src/main/java/com/wdiscute/starcatcher/minigame/FishingMinigameScreen.java
@@ -99,7 +99,7 @@ public class FishingMinigameScreen extends Screen implements GuiEventListener
 
 
 
-    // Nikdo53 values, these are mine dont steal them
+    // Nikdo53 fields, these are mine dont steal them
     public final int holdingDelay = 6;
     public int holdingTicks = 0;
     protected boolean isHoldingKey = false;
@@ -108,6 +108,8 @@ public class FishingMinigameScreen extends Screen implements GuiEventListener
     public float renderScale;
 
     protected final List<ActiveSweetSpot> activeSweetSpots = new ArrayList<>();
+    protected final List<ActiveSweetSpot> spotsToAdd = new ArrayList<>(); // delays the adding process to avoid concurrency exceptions
+
     protected final List<AbstractMinigameModifier> modifiers = new ArrayList<>();
 
     public FishingMinigameScreen(FishProperties fp, ItemStack rod)
@@ -222,7 +224,7 @@ public class FishingMinigameScreen extends Screen implements GuiEventListener
             ass = modifier.onSpotAdded(ass);
         }
 
-        if (!ass.removed) this.activeSweetSpots.add(ass);
+        if (!ass.removed) this.spotsToAdd.add(ass);
     }
 
     public int getRandomFreePosition(int sizeOfTheSweetspotToPlace)
@@ -576,6 +578,9 @@ public class FishingMinigameScreen extends Screen implements GuiEventListener
             if (s.removed) s.behaviour.onRemove();
             return s.removed;
         });
+
+        activeSweetSpots.addAll(spotsToAdd);
+        spotsToAdd.clear();
 
         //remove modifiers marked for removal
         modifiers.removeIf(m ->
