ce7f36d63eeac72d473c3b7a7eedd792cca94157
wd <lucasalvesbispo2@gmail.com>
2026-02-14 15:05:27 +0000

    rebase of flip every hit

--- src/main/java/com/wdiscute/starcatcher/registry/custom/minigamemodifiers/FlipEveryHitModifier.java
+++ src/main/java/com/wdiscute/starcatcher/registry/custom/minigamemodifiers/FlipEveryHitModifier.java
@@ -1,9 +1,36 @@
 package com.wdiscute.starcatcher.registry.custom.minigamemodifiers;
 
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.wdiscute.starcatcher.minigame.ActiveSweetSpot;
+import net.neoforged.neoforge.registries.DeferredHolder;
 
-public class FlipEveryHitModifier extends AbstractMinigameModifier
+import java.util.function.Supplier;
+
+public class FlipEveryHitModifier extends AbstractTimedModifier
 {
+    public static final MapCodec<FlipEveryHitModifier> CODEC = RecordCodecBuilder.mapCodec(instance ->
+            instance.group(
+                    Codec.INT.optionalFieldOf("length", -1).forGetter(AbstractTimedModifier::getLength)
+            ).apply(instance, FlipEveryHitModifier::new));
+
+    public FlipEveryHitModifier(int length) {
+        super(length);
+    }
+
+    @Override
+    public MapCodec<? extends AbstractMinigameModifier> codec()
+    {
+        return CODEC;
+    }
+
+    @Override
+    public DeferredHolder<Supplier<AbstractMinigameModifier>, Supplier<AbstractMinigameModifier>> getRegistryHolder()
+    {
+        return ModMinigameModifiers.FLIP_EVERY_HIT;
+    }
+
     @Override
     public boolean onHit(ActiveSweetSpot ass)
     {
