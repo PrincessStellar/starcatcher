38f2346ead3ceffa3965a52c0f4085ea0cdfe935
wd <lucasalvesbispo2@gmail.com>
2026-04-21 20:53:04 +0100

    v2.3.3 publish, bumped Nikdos TML, made treasures use the treasure loot table

--- src/main/java/com/wdiscute/starcatcher/SCConfig.java
+++ src/main/java/com/wdiscute/starcatcher/SCConfig.java
@@ -56,7 +56,7 @@ public class SCConfig
             .comment("Whether to use the vanilla partial ticks for minigame smoothing or a custom implementation from 1.20")
             .comment("Vanilla should look better for most people")
             .translation("starcatcher.configuration.vanilla_partial_ticks")
-            .define("vanilla_partial_ticks", true);
+            .define("vanilla_partial_ticks", false);
 
 
     static final ModConfigSpec SPEC = BUILDER_CLIENT.build();
